d5419a9be1058ad51063a6b8af18aabe5555c5f4
Erikson <chris.s.erickson@gmail.com>
2022-09-15 06:01:54 -0400

    added logout.js to main for logout button

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -91,6 +91,17 @@ router.post('/login', (req, res) => {
     });
   });
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 // delete
 router.delete("/:id", (req, res) => {
   User.destroy({
