7dd82570f0366c1e02e8db07f42d99abc9c3e65c
Erikson <chris.s.erickson@gmail.com>
2022-09-15 04:13:20 -0400

    user is now saved to DB from signup

--- server.js
+++ server.js
@@ -33,10 +33,6 @@ app.use(controllers);
 
 // express.static -- middleware takes all content in folder and serve as static assets
 app.use(express.static(path.join(__dirname, "public")));
-// // // express handlebars
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
-
 // turn on connection to db and server
 // sync means that this is a sequalize taking the models and connecting them to associated database tables.
 // force: true is the same as MySQL DROP TABLE IF EXISTS ... Used when a model has been changed
